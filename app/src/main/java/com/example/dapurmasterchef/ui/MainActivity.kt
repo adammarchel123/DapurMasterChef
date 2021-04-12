0d03ea6e95896ce643a4063694d5e738302ffa06
adammarchel123 <adammarchel123@gmail.com>
2021-04-12 11:20:26 +0700

    finish2

--- app/src/main/java/com/example/dapurmasterchef/ui/MainActivity.kt
+++ app/src/main/java/com/example/dapurmasterchef/ui/MainActivity.kt
@@ -8,20 +8,16 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.dapurmasterchef.R
-import com.example.dapurmasterchef.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    // private lateinit var binding: ActivityMainBinding
-
+    
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
 
@@ -34,8 +30,9 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
